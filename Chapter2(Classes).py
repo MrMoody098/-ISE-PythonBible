01cac16c3e1377e665403cac75c74bb8e691785b
MrMoody098 <122175726+MrMoody098@users.noreply.github.com>
2024-01-23 14:44:49 +0000

    added comments to Daemon file

--- Chapter2(Classes).py
+++ Chapter2(Classes).py
@@ -1,67 +1,78 @@
-#Classes and Objects
+# Classes and Objects
 class Car:
-    #class variable accesible from all instances
+    # class variable accesible from all instances
     amount_cars = 0
-    #this is our constructer used for initialising insitances of a class
-    def __init__(self,manufacturer,model,hp):
+
+    # this is our constructer used for initialising insitances of a class
+    def __init__(self, manufacturer, model, hp):
         self.manufacturer = manufacturer
         self.model = model
         self.hp = hp
-        Car.amount_cars+=1
-        #hidden attributes are only accesible within the class .ie they are private
+        Car.amount_cars += 1
+        # hidden attributes are only accesible within the class .ie they are private
         self.__hidden = "Hello"
 
-    #this is our destructer this is called when an isntance is deleted
+    # this is our destructer this is called when an isntance is deleted
     def __del__(self):
         print("Car deleted")
-        Car.amount_cars -=1
+        Car.amount_cars -= 1
 
     def print_info(self):
-        print("Manufacturer: {}, Model: {}, HP: {}".format(self.manufacturer,self.model,self.hp))
+        print("Manufacturer: {}, Model: {}, HP: {}".format(self.manufacturer, self.model, self.hp))
 
 
-jimmy = Car("ee","22",12)
+jimmy = Car("ee", "22", 12)
 jimmy.print_info()
 print(Car.amount_cars)
 del jimmy
 print(Car.amount_cars)
 
-class Person:
-    def __init__(self,name,age):
+
+class Person:  # super class
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-    def getOlder(self,years):
+
+    def getOlder(self, years):
         self.age += years
 
-class Programmer(Person):
-    def __init__(self,name,age,language):
-        super(Programmer,self).__init__(name,age)
+
+class Programmer(Person):  # child class
+    def __init__(self, name, age, language):
+        super(Programmer, self).__init__(name, age)  # superfunction inhertes from superclass
         self.language = language
+
     def favLanguage(self):
         print("My favorite programming language is {}".format(self.language))
 
-john = Programmer("John",20,"C++")
+
+john = Programmer("John", 20, "C++")
 print(john.age)
 john.favLanguage()
 john.getOlder(2)
 print(john.age)
 
+
 class Vector:
-    def __init__(self,x,y):#constucter
+    def __init__(self, x, y):  # constucter
         self.x = x
         self.y = y
-    def __add__(self, other):#defines what happens when 2 Vector Objects are added together
-        return Vector(self.x+other.x,
-                      self.y+other.y)
-    def __sub__(self, other):#defines what happens when 2 Vector Objects are subtracted
-        return Vector(self.x+other.x,
-                      self.y+other.y)
-    def __str__(self):#Defines what happens when we print our Vector
-        return "x: {} y: {}".format(self.x,self.y)
 
-v1 = Vector(1,2)
-v2 = Vector(2,4)
-v3 = v1+v2
+    def __add__(self, other):  # defines what happens when 2 Vector Objects are added together
+        return Vector(self.x + other.x,
+                      self.y + other.y)
+
+    def __sub__(self, other):  # defines what happens when 2 Vector Objects are subtracted
+        return Vector(self.x + other.x,
+                      self.y + other.y)
+
+    def __str__(self):  # Defines what happens when we print our Vector
+        return "x: {} y: {}".format(self.x, self.y)
+
+
+v1 = Vector(1, 2)
+v2 = Vector(2, 4)
+v3 = v1 + v2
 print(v1)
 print(v2)
 print(v3)
--- DeamonThreads.py
+++ DeamonThreads.py
@@ -1,27 +1,40 @@
-#daemon Threads are a special kind of thread that runs in the background this means that the
-#program can be terminated even if this thread is still running4
-
+# daemon Threads are a special kind of thread that runs in the background this means that the
+# program can be terminated even if this thread is still running4
 import threading
 import time
 
+# Define the path to the file
 path = "C:/Users/ticta/OneDrive/Desktop/file.txt"
+# Initialize an empty string to store the text from the file
 text = ""
 
+# Function to read the file
 def readFile():
-    global path,text
+    # Declare global variables
+    global path, text
+    # Continuously read the file
     while True:
+        # Open the file and read its content
         with open(path) as file:
             text = file.read()
+        # Wait for 3 seconds before reading the file again
         time.sleep(3)
 
+# Function to print the text from the file
 def printLoop():
+    # Declare global variable
     global text
+    # Print the text 30 times
     for x in range(30):
         print(text)
+        # Wait for 1 second before printing again
         time.sleep(1)
 
-t1 = threading.Thread(target=readFile,daemon=True)
+# Create a daemon thread to read the file
+t1 = threading.Thread(target=readFile, daemon=True)
+# Create a thread to print the text
 t2 = threading.Thread(target=printLoop)
 
+# Start the threads
 t1.start()
 t2.start()
